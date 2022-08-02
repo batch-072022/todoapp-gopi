50e7707c06267c2170921012929d8448252796c2
Gopinadh Kandepu <gopinadh.kandepu@valuemomentum.com>
2022-08-02 10:29:09 +0530

    committing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { ListTaskComponent } from './list-task/list-task.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { HeaderComponent } from './header/header.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddTaskComponent,
+    ForgetPasswordComponent,
+    ListUsersComponent,
+    ViewProfileComponent,
+    ListTaskComponent,
+    ChangePasswordComponent,
+    HeaderComponent,
+    EditTaskComponent
   ],
   imports: [
     BrowserModule,
